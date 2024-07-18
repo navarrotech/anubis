7442823ac2097840f65b0ed5600b2a075bc721ea
Alex Navarro <alex@navarrocity.com>
2024-07-17 23:03:47 -0600

    Create directories checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,25 @@
 // Copyright © 2024 Navarrotech
 
-use clap::{Parser, Args};
+use clap::Parser;
+use crate::cli::{InitArgs, init};
 
 #[derive(Parser)]
+#[clap(version = "1.0", author = "Alex Navarro")]
 #[command(name = "cargo", bin_name = "cargo")]
 enum CargoCli {
-    ExampleDerive(ExampleDeriveArgs),
     Init(InitArgs),
 }
 
-#[derive(Args)]
-#[command(version, about, long_about = None)]
-struct ExampleDeriveArgs {
-    #[arg(long)]
-    manifest_path: Option<std::path::PathBuf>,
-}
-
-#[derive(Args)]
-struct InitArgs {
-    // Add any arguments you need for the init command here
-}
-
-fn init(_args: InitArgs) {
-    println!("Running init command");
-    // Add your initialization logic here
-}
-
-fn main() {
+fn main() -> std::io::Result<()> {
     let cli = CargoCli::parse();
 
     match cli {
-        CargoCli::ExampleDerive(args) => {
-            println!("{:?}", args.manifest_path);
-        }
         CargoCli::Init(args) => {
-            init(args);
+            init(args)?;
         }
     }
+
+    Ok(())
 }
+
+mod cli;
