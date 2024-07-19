a9f259e404944635227042667bfb52831d8c2ec8
Alex Navarro <alex@navarrocity.com>
2024-07-18 21:02:30 -0600

    Fixing clippy issues

--- src/cli/init/setup.rs
+++ src/cli/init/setup.rs
@@ -33,7 +33,7 @@ mod setup_tests {
         let temp_dir = tempdir().unwrap();
         let base_path = temp_dir.path();
 
-        setup_directories(&base_path).unwrap();
+        setup_directories(base_path).unwrap();
 
         for &dir in DIRS {
             let path = temp_dir.path().join(dir);
@@ -46,7 +46,7 @@ mod setup_tests {
         let temp_dir = tempdir().unwrap();
         let base_path = temp_dir.path().join("test_subdir");
 
-        setup_directories(&base_path).unwrap();
+        setup_directories(base_path.as_path()).unwrap();
 
         for &dir in DIRS {
             let path = temp_dir.path().join("test_subdir").join(dir);
